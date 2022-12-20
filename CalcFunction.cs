1338bcff63e0bc257938c01b054da01c1553615a
Juneeee <95747661+xzono0002@users.noreply.github.com>
2022-12-20 12:39:14 +0800

    Add project files.

---
+++ CalcFunction.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalcFunction
+    {
+        public double calcFunction(double num1, double num2, int op)
+        {
+            double result = 0.0;
+
+            switch (op)
+            {
+                case 1:
+                    result = num1 + num2;
+                    break;
+                case 2:
+                    result = num1 - num2;
+                    break;
+                case 3:
+                    result = num1 * num2;
+                    break;
+                case 4:
+                    result = num1 / num2;
+                    break;
+                case 5:
+                    Console.WriteLine("Calculator is closing...");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. Please try again!");
+                    break;
+            }
+
+            return result;
+        }
+
+        public void menu()
+        {
+            Console.WriteLine("MENU");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.WriteLine("5. Exit");
+            Console.Write("Please input the number of your answer: ");
+        }
+
+        public double calcBody(double num1, double num2, int choice1)
+        {
+            double result = 0.0;
+            Console.Write("Enter your first number: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter your second number: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Result: " + calcFunction(num1, num2, choice1));
+            Console.WriteLine();
+            return result;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        static void Main(String[] args)
+        {
+
+            //Declaration of Variables
+
+            int choice;
+            double num1 = 0;
+            double num2 = 0;
+
+            CalcFunction calc = new CalcFunction();
+
+
+            //loop using do-while
+
+            do
+            {
+                calc.menu();
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    calc.calcBody(num1, num2, choice);
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+                else if (choice == 2)
+                {
+                    calc.calcBody(num1, num2, choice);
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+                else if (choice == 3)
+                {
+                    calc.calcBody(num1, num2, choice);
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+                else if (choice == 4)
+                {
+                    calc.calcBody(num1, num2, choice);
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+                else if (choice == 5)
+                {
+                    calc.calcFunction(num1, num2, choice);
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+                else
+                {
+                    calc.calcFunction(num1, num2, choice);
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+            }
+
+            while (choice != 5);
+           
+        }
+        
+    }
+}   
